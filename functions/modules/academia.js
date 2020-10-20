01f393f442374dac04c57b883efb88e596d6e70f
Thiago P. Martinez <thiago.martinez@nuvem.net>
2020-10-19 21:48:56 -0400

    chore: Removido a função da classe

--- functions/modules/academia.js
+++ functions/modules/academia.js
@@ -32,39 +32,36 @@ class AcademiaModule {
             console.log(`DATE=${date} TIME=${time} NAME=${given_name}`)
 
             try {
-                await reservar(props, date_parsed, time_parsed)
+                const browser = await puppeteer.launch({headless: true})
+
+                const page = (await browser.pages()).shift()
+                await page.setViewport({width: 1366, height: 768})
+                await page.goto('https://www.smartfit.com.br/person_sessions/new', {waitUntil: 'networkidle2'})
+                
+                // Type CPF, Password and submit
+                await page.type('#login', props.cpf, {delay: 50})
+                await page.type('#person_session_password', props.password, {delay: 50})
+                await page.click('#s_login')
+
+                await page.waitForSelector('a.js-booking-button')
+
+                await page.click('a.js-booking-button')
+                await page.waitForTimeout(3000)
+
+                const reservasPage = (await browser.pages()).pop()
+                await reservasPage.goto(`http://reservas.smartfit.com.br/klasses?date=${date_parsed.toLocaleDateString()}&location_acronym=MTCGOI1&utf8=%E2%9C%93`, {waitUntil: 'networkidle2'})
+                await reservasPage.waitForSelector('.Card__item')
+
+                const [button] = await reservasPage.$x(`//div[contains(@class, "Card__item") and contains(., "${date_parsed.getDate()}/${date_parsed.getMonth() + 1} ${time_parsed.getHours()}:${time_parsed.getMinutes()}")]/div[3]/a`)
+                await button.click()
+
+                await browser.close()
                 conv.ask('Pronto, foi reservado !')
             } catch (e) {
                 console.error(e)
                 conv.ask('Ocorreu um erro ao solicitar a reserva. Tenta novamente mais tarde !')
             }
         })
-    }
-
-    async reservar(props, date, time) {
-        const browser = await puppeteer.launch({headless: true})
-        const page = (await browser.pages()).shift()
-        await page.setViewport({width: 1366, height: 768})
-        await page.goto('https://www.smartfit.com.br/person_sessions/new', {waitUntil: 'networkidle2'})
-        
-        // Type CPF, Password and submit
-        await page.type('#login', props.cpf, {delay: 50})
-        await page.type('#person_session_password', props.password, {delay: 50})
-        await page.click('#s_login')
-    
-        await page.waitForSelector('a.js-booking-button')
-    
-        await page.click('a.js-booking-button')
-        await page.waitForTimeout(3000)
-    
-        const reservasPage = (await browser.pages()).pop()
-        await reservasPage.goto(`http://reservas.smartfit.com.br/klasses?date=${date.toLocaleDateString()}&location_acronym=MTCGOI1&utf8=%E2%9C%93`, {waitUntil: 'networkidle2'})
-        await reservasPage.waitForSelector('.Card__item')
-    
-        const [button] = await reservasPage.$x(`//div[contains(@class, "Card__item") and contains(., "${date.getDate()}/${date.getMonth() + 1} ${time.getHours()}:${time.getMinutes()}")]/div[3]/a`)
-        await button.click()
-    
-        await browser.close()
     }
 
 }
